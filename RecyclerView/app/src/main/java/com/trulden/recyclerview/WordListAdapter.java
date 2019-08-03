f90a05a95a16ab5527d564b5753bb64dce167a96
Kirill <thegaldren@gmail.com>
2019-08-03 11:35:44 +0300

    complete RecyclerView

--- RecyclerView/app/src/main/java/com/trulden/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/trulden/recyclerview/WordListAdapter.java
@@ -1,29 +1,41 @@
 package com.trulden.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
+
 public class WordListAdapter extends
         RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
 
+    public WordListAdapter(Context context, LinkedList<String> wordList){
+        mInflater = LayoutInflater.from(context);
+        mWordList = wordList;
+    }
 
     @NonNull
     @Override
     public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mItemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
-
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
     }
 
     class WordViewHolder extends RecyclerView.ViewHolder{
