56734b66eed0e0aa038e44290a47876cb55fa468
Kirill <thegaldren@gmail.com>
2019-08-03 11:35:24 +0300

    rearrange

--- RecyclerView/app/src/main/java/com/trulden/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/trulden/recyclerview/WordListAdapter.java
@@ -9,6 +9,23 @@ import androidx.recyclerview.widget.RecyclerView;
 public class WordListAdapter extends
         RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     class WordViewHolder extends RecyclerView.ViewHolder{
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
@@ -18,21 +35,5 @@ public class WordListAdapter extends
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
         }
-    }
-
-    @NonNull
-    @Override
-    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
     }
 }
