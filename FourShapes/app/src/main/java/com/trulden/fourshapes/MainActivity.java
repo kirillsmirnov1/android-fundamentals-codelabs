97bd23912d2a2c671559be032fb95969f67bcc3f
Kirill <thegaldren@gmail.com>
2019-08-06 12:37:40 +0300

    fuck it

--- FourShapes/app/src/main/java/com/trulden/fourshapes/MainActivity.java
+++ FourShapes/app/src/main/java/com/trulden/fourshapes/MainActivity.java
@@ -1,5 +1,8 @@
 package com.trulden.fourshapes;
 
+import android.transition.Explode;
+import android.view.View;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +11,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // inside your activity (if you did not enable transitions in your theme)
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         setContentView(R.layout.activity_main);
+    }
+
+    public void explode(View view) {
+
+        // set an exit transition
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setExitTransition(new Explode());
+
+        recreate();
     }
 }
