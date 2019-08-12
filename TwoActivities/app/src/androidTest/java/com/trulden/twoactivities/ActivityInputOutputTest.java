3b202d8d1111a793e6cb2a8d486431828a5238f1
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-12 17:40:52 +0300

    add another test

--- TwoActivities/app/src/androidTest/java/com/trulden/twoactivities/ActivityInputOutputTest.java
+++ TwoActivities/app/src/androidTest/java/com/trulden/twoactivities/ActivityInputOutputTest.java
@@ -10,9 +10,9 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -40,5 +40,12 @@ public class ActivityInputOutputTest {
         onView(withId(R.id.text_header)).check(matches(isDisplayed()));
         onView(withId(R.id.button_second)).perform(click());
         onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.editText_main)).perform(typeText("This is a test."));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("This is a test.")));
     }
 }
