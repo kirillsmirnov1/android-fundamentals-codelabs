ce92cb3ba426ee86cdc11f86e149779eda9776c3
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-13 14:28:07 +0300

    add receiver

--- PowerReceiver/app/src/main/java/com/trulden/powerreceiver/CustomReceiver.java
+++ PowerReceiver/app/src/main/java/com/trulden/powerreceiver/CustomReceiver.java
@@ -3,13 +3,26 @@ package com.trulden.powerreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String intentAction = intent.getAction();
+
+        if(intentAction != null){
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected!";
+                    break;
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
