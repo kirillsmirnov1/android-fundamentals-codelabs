5a69acb2e6b6ae01ca98c1e52476dc479235bbc6
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-13 14:43:40 +0300

    add local broadcast and receiver

--- PowerReceiver/app/src/main/java/com/trulden/powerreceiver/MainActivity.java
+++ PowerReceiver/app/src/main/java/com/trulden/powerreceiver/MainActivity.java
@@ -3,12 +3,16 @@ package com.trulden.powerreceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         // Register the receiver using the activity context.
         this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
     }
 
     @Override
     protected void onDestroy() {
         this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
         super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
     }
 }
