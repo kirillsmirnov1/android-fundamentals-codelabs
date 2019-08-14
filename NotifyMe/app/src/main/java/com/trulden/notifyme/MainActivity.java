4bc69443b8776837017cf770076b0dd7a978315c
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-14 10:05:44 +0300

    add NotifyMe

---
+++ NotifyMe/app/src/main/java/com/trulden/notifyme/MainActivity.java
@@ -0,0 +1,66 @@
+package com.trulden.notifyme;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonNotify;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final int NOTIFICATION_ID = 0;
+    private NotificationManager mNotifyManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButtonNotify = findViewById(R.id.notify);
+        mButtonNotify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+
+        createNotificationChannel();
+    }
+
+    public void sendNotification(){
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    public void createNotificationChannel(){
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder =
+                new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                        .setContentTitle("You've been notified!")
+                        .setContentText("This is your notification text.")
+                        .setSmallIcon(R.drawable.ic_android);
+        return notifyBuilder;
+
+
+    }
+}
