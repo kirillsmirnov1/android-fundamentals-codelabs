981f1c9248b0a6097669073c108c1ce09f03ef38
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-19 11:26:12 +0300

    add annotations to Word class

--- RoomWordsSample/app/src/main/java/Word.java
+++ RoomWordsSample/app/src/main/java/Word.java
@@ -1,6 +1,14 @@
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "word_table")
 public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(@NonNull String word){
