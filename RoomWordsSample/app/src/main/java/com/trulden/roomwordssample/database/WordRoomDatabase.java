4029642a6e858ab1debd3b731033a5a5eba61aa8
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-19 12:41:31 +0300

    add callback destroying old and creating new data

--- RoomWordsSample/app/src/main/java/com/trulden/roomwordssample/database/WordRoomDatabase.java
+++ RoomWordsSample/app/src/main/java/com/trulden/roomwordssample/database/WordRoomDatabase.java
@@ -1,14 +1,26 @@
 package com.trulden.roomwordssample.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import androidx.annotation.NonNull;
 import androidx.room.*;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
 
     private static WordRoomDatabase INSTANCE;
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
 
     public static WordRoomDatabase getDatabase(final Context context){
         if(INSTANCE == null){
@@ -18,6 +30,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
@@ -43,4 +56,29 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     }
 
     public abstract WordDao wordDao();
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final WordDao mDao;
+        String[] words = {"Dolphin", "Crocodile", "Cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db){
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for(String word : words){
+                mDao.insert(new Word(word));
+            }
+
+            return null;
+        }
+
+    }
 }
