3a4ea7ebeab73ffa98a3687e5eda15d7ed8d3ccd
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-19 10:30:00 +0300

    add annotations to Person class

--- DatabaseExample/app/src/main/java/com/trulden/databaseexample/Person.java
+++ DatabaseExample/app/src/main/java/com/trulden/databaseexample/Person.java
@@ -1,7 +1,44 @@
 package com.trulden.databaseexample;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
 public class Person {
+
+    @PrimaryKey (autoGenerate = true)
     private int uid;
+
+    @ColumnInfo (name = "first_name")
     private String firstName;
+
+    @ColumnInfo (name = "last_name")
     private String lastName;
+
+    // Getters and setters
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
