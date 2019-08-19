3a3697786e7c2bc97875a98dc81022748e89e624
kirillsmirnov1 <thegaldren@gmail.com>
2019-08-19 12:13:55 +0300

    create WordViewModel

---
+++ RoomWordsSample/app/src/main/java/com/trulden/roomwordssample/WordViewModel.java
@@ -0,0 +1,11 @@
+package com.trulden.roomwordssample;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class WordViewModel extends AndroidViewModel {
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
